d7d5fb308463685a7384cad9f7b1fec400f0a75c
wavybangsy <mary_erika_culala@dlsu.edu.ph>
2024-04-02 02:20:21 +0800

    fixed bounds checker

--- kernelC.c
+++ kernelC.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 void print_c_result(double *Y, int n){
     int k = 10;
@@ -28,21 +29,31 @@ void print_c_result(double *Y, int n){
 }
 
 
-void c_stencil(int n, double *X, double *Y){
-    for (int i = 0; i < n; i++){
-        Y[i] = 0.0;
+void c_stencil(int n, double *X, double *Y, int *new_n) {
+    int flag = 0;
+    int y_count = 0;
+
+    for (int i = 0; i < n; i++) {
         double sum = 0;
 
-        for (int offset = -3; offset <=3; offset++){
-
+        for (int offset = -3; offset <= 3; offset++) {
             int index = i + offset;
             if (index >= 0 && index < n) {
                 sum += X[index];
+                flag = 0;
+            } else {
+                flag = 1;
+                break;
             }
         }
-        Y[i] = sum;
+        if (flag == 0) {
+            Y[y_count] = sum; 
+            y_count++;
+            *new_n = y_count;
+        }
     }
 }
+
 
 
 
@@ -54,14 +65,17 @@ void c_stencil(int n, double *X, double *Y){
 int main(){
 
     // get input n from user
-    const int sizes[] = {1048576, 16777216, 268435456};
+    const int sizes[] = {1048576, 16777216, 134217728};
+    // const int sizes[] = {8};
     const int num_sizes = sizeof(sizes) / sizeof(int);
     int num_runs = 35;
+    int new_n = 0;
 
     printf("\n");
+    printf("==============================================================\n");
     for (int size = 0; size < num_sizes; size++){
         int n = sizes[size];
-        printf("Vector size: %d\n\n", n);
+        printf("Vector size: 2^%.f\n\n", log2(n));
     
         double* Y = malloc(n * sizeof(double)); 
         double* X = malloc(n * sizeof(double)); 
@@ -79,7 +93,7 @@ int main(){
         // call c kernel
         clock_t start_time = clock();
         for (int i = 0; i < num_runs; i++) {
-            c_stencil(n, X, Y);
+            c_stencil(n, X, Y, &new_n);
         }
         clock_t end_time = clock();
         double c_elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC / num_runs;
@@ -100,7 +114,7 @@ int main(){
         // double c_elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC / num_runs;
         // printf("Assembly Kernel: Average time taken: %f seconds\n", c_elapsed_time);
 
-        print_c_result(Y, n);
+        print_c_result(Y, new_n);
         printf("==============================================================\n");
 
         free(Y);
