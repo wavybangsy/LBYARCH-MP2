857f487a86146fc06f1b63e0c7f2ccdb40ae18d4
wavybangsy <mary_erika_culala@dlsu.edu.ph>
2024-04-02 22:21:40 +0800

    main added with assembly caller

--- main.c
+++ main.c
@@ -2,25 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <windows.h>
 
-void print_c_result(double *Y, int n){
+extern void asmhello(int n, double* x, double* y, int* new_n);
+
+void print_result(double* Y, int n) {
     int k = 10;
 
 
-    printf("C kernel: Y = [");
-    if (n <= 10){
-        for (int j = 0; j < n; j++){
-            if (j == n-1){
+    printf("Y = [");
+    if (n <= 10) {
+        for (int j = 0; j < n; j++) {
+            if (j == n - 1) {
                 printf("%.lf", Y[j]);
-            } else {
+            }
+            else {
                 printf("%.lf, ", Y[j]);
             }
         }
-    } else if (n > 10){
-        for (int j = 0; j < k; j++){
-            if (j == k-1){
+    }
+    else if (n > 10) {
+        for (int j = 0; j < k; j++) {
+            if (j == k - 1) {
                 printf("%.lf", Y[j]);
-            } else {
+            }
+            else {
                 printf("%.lf, ", Y[j]);
             }
         }
@@ -29,7 +35,7 @@ void print_c_result(double *Y, int n){
 }
 
 
-void c_stencil(int n, double *X, double *Y, int *new_n) {
+void c_stencil(int n, double* X, double* Y, int* new_n) {
     int flag = 0;
     int y_count = 0;
 
@@ -41,13 +47,14 @@ void c_stencil(int n, double *X, double *Y, int *new_n) {
             if (index >= 0 && index < n) {
                 sum += X[index];
                 flag = 0;
-            } else {
+            }
+            else {
                 flag = 1;
                 break;
             }
         }
         if (flag == 0) {
-            Y[y_count] = sum; 
+            Y[y_count] = sum;
             y_count++;
             *new_n = y_count;
         }
@@ -55,70 +62,62 @@ void c_stencil(int n, double *X, double *Y, int *new_n) {
 }
 
 
-
-
-/**
- * TODO: Call extern *assembly_function_name*
-*/
-
-
-int main(){
+int main() {
 
     // get input n from user
-    const int sizes[] = {1048576, 16777216, 134217728};
+    const int sizes[] = { 1048576,  16777216, 134217728 };
     // const int sizes[] = {8};
     const int num_sizes = sizeof(sizes) / sizeof(int);
     int num_runs = 35;
-    int new_n = 0;
+    int* new_n = 0;
 
     printf("\n");
     printf("==============================================================\n");
-    for (int size = 0; size < num_sizes; size++){
+    for (int size = 0; size < num_sizes; size++) {
         int n = sizes[size];
         printf("Vector size: 2^%.f\n\n", log2(n));
-    
-        double* Y = malloc(n * sizeof(double)); 
-        double* X = malloc(n * sizeof(double)); 
 
-        if (Y == NULL || X == NULL) {
+        double* y = (double*)malloc(n * sizeof(double));
+        double* x = (double*)malloc(n * sizeof(double));
+
+        if (x == NULL || y == NULL) {
             printf("Memory allocation failed.\n");
             return 0;
         }
 
         // initialize X array to store 1 to num
-        for (int count = 0; count < n; count++){
-            X[count] = count + 1;
+        for (int count = 0; count < n; count++) {
+            x[count] = count + 1;
         }
 
         // call c kernel
         clock_t start_time = clock();
         for (int i = 0; i < num_runs; i++) {
-            c_stencil(n, X, Y, &new_n);
+            c_stencil(n, x, y, &new_n);
         }
         clock_t end_time = clock();
         double c_elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC / num_runs;
         printf("C Kernel: Average time taken: %f seconds\n", c_elapsed_time);
+
+        printf("C Kernel: ");
+        print_result(y, new_n);
 
 
         /**
         * TODO: Uncomment for assembly
         */
         // call assembly kernel
-        // clock_t start_time = clock();
-        // for (int i = 0; i < num_runs; i++) {
-            /**
-             * TODO: call assembly function here
-            */
-        // }
-        // clock_t end_time = clock();
-        // double c_elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC / num_runs;
-        // printf("Assembly Kernel: Average time taken: %f seconds\n", c_elapsed_time);
+        clock_t start_time2 = clock();
+        for (int i = 0; i < num_runs; i++) {
+            asmhello(n, x, y, &new_n);
+        }
+        clock_t end_time2 = clock();
+        double c_elapsed_time2 = ((double)(end_time2 - start_time2)) / CLOCKS_PER_SEC / num_runs;
+        printf("Assembly Kernel: Average time taken: %f seconds\n", c_elapsed_time2);
 
-        print_c_result(Y, new_n);
+        printf("Assembly Kernel: ");
+        print_result(y, new_n);
         printf("==============================================================\n");
-
-        free(Y);
-        free(X);
     }
     return 0;
 }
